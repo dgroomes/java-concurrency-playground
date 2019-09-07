930bb848085542d4d169eefb3c41aaa09fc38ec9
davidgroomes <dwgroomes@gmail.com>
2019-09-07 16:41:33 -0500

    Improve clarity of interrupts example

--- interrupts/src/dgroomes/InterruptsMain.java
+++ interrupts/src/dgroomes/InterruptsMain.java
@@ -13,8 +13,8 @@ public class InterruptsMain {
         var thread = new Thread(() -> {
             log.info("Hello from a thread!");
             try {
-                log.info("Sleeping for a second");
-                Thread.sleep(1000);
+                log.info("Sleeping for five seconds");
+                Thread.sleep(5000);
                 log.info("Done sleeping");
             } catch (InterruptedException e) {
                 log.warning("I was interrupted");
@@ -22,8 +22,8 @@ public class InterruptsMain {
         });
 
         thread.start();
-        Thread.sleep(500);
-        log.info("Interrupting the thread");
+        Thread.sleep(2000);
+        log.info("Interrupting the thread before it can finish");
         thread.interrupt();
         log.info("Waiting for the thread to die");
         thread.join();
