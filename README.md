0330bebdfc64f76a06ca85d6ddd0608b3a7eba55
David Groomes <dwgroomes@gmail.com>
2020-04-25 00:28:46 -0500

    Update to virtual threads (goodbye and so long fibers!)

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 Learning and exploring concurrency in Java. Using examples from <https://docs.oracle.com/javase/tutorial/essential/concurrency/index.html>
 
-### `fibers/` (should be renamed _loom_. _Fibers_ are out; _Virtual Threads_ are in since 11/2019)
+### `loom/`
 
 Trying out _Project_ Loom and the new programming paradigm for Java using *user-mode threads*.
 
 REQUIRES an experimental JDK built from the OpenJDK Project Loom branch <https://wiki.openjdk.java.net/display/loom/Main>.
 
-Run it with `java fibers/src/dgroomes/FibersMain.java`
+Run it with `java loom/src/dgroomes/VirtualThreadsMain.java`
 
 ### `interrupts/`
 
---
+++ loom/src/dgroomes/VirtualThreadsMain.java
@@ -0,0 +1,47 @@
+package dgroomes;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.logging.Logger;
+
+/**
+ * REQUIRES an experimental JDK built from the OpenJDK Project Loom branch https://wiki.openjdk.java.net/display/loom/Main
+ *
+ * Reference my notes for building the JDK on macOS https://gist.github.com/dgroomes/3af073b71c2c34581a155af9daa86564
+ */
+public class VirtualThreadsMain {
+
+    private static Logger log = Logger.getAnonymousLogger();
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tF %1$tT %4$s %2$s %5$s%6$s%n");
+        log.info("Hello! Can we implement something with Virtual Threads (OpenJDK Project Loom)?");
+
+        Callable<String> task1 = () -> {
+            for (int i = 0; i < 2; i++) {
+                log.info("Hello!");
+                Thread.sleep(Duration.of(1, ChronoUnit.SECONDS));
+            }
+            return "task 1";
+        };
+        Callable<String> task2 = () -> {
+            Thread.sleep(Duration.of(500, ChronoUnit.MILLIS));
+            for (int i = 0; i < 2; i++) {
+                log.info("\t\tHi!");
+                Thread.sleep(Duration.of(1, ChronoUnit.SECONDS));
+            }
+            return "task 2";
+        };
+
+        try (ExecutorService executor = Executors.newUnboundedVirtualThreadExecutor()) {
+            executor.submitTasks(List.of(task1, task2));
+            executor.shutdown();
+            log.info("Wait for the tasks to execute to completion");
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+        }
+
+        log.info("Done");
+    }
+}
